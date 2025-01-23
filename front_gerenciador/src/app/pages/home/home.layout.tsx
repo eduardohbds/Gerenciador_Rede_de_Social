3f0e3b8918103fe8660ca66914c12d9bb78002ec
eduardohbds <eduardo.hbda@gmail.com>
2025-01-23 19:23:07 -0300

    feat: created the home page and layout

---
+++ front_gerenciador/src/app/pages/home/home.layout.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export const HomeLayout = ({
+  children,
+}:{
+  children:React.ReactNode
+}) => {
+  return(
+    <html lang="en">
+      <body>
+        {/* Layout UI */}
+        {/* Place children where you want to render a page or nested layout */}
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
